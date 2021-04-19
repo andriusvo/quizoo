316910070b9eaa8f508fcb384dbf3e86688ddae0
Andrejus Voitovas <andrejus.voitovas@nfq.lt>
2021-04-19 20:51:08 +0300

    Configured env file, added scripts.

---
+++ tools/docker/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+DEPLOY_NAME="$2"
+PROJECT_ROOT="$(dirname $(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)))"
+echo -e "Triggering ${DEPLOY_NAME} deploy\n\n"
+
+function setPerms {
+	mkdir -p $1
+	sudo setfacl -R  -m m:rwx -m u:33:rwX -m u:1000:rwX $1
+	sudo setfacl -dR -m m:rwx -m u:33:rwX -m u:1000:rwX $1
+}
+
+echo -e '\n## Install mage ... '
+mkdir -p ${PROJECT_ROOT}/tools/mage
+cd  ${PROJECT_ROOT}/tools/mage
+composer init -n
+composer --no-interaction config github-oauth.github.com 3b6e5f518a2ffb7399c2b8be8629e91f8feb5821
+composer --no-interaction require 'andres-montanez/magallanes' '^4.0'
+
+echo -e '\n## Setting up permissions ... '
+setPerms "${PROJECT_ROOT}/var"
+setPerms "${PROJECT_ROOT}/var/log"
+
+cd ${PROJECT_ROOT}
+
+APP_ENV=prod composer install --no-dev --optimize-autoloader --classmap-authoritative --no-interaction
+APP_ENV=prod tools/mage/vendor/bin/mage deploy "${DEPLOY_NAME}"
---
+++ tools/docker/develop_init.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+set -x
+
+PROJECT_ROOT="$(dirname $(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)))"
+
+echo "PROJECT ROOT: ${PROJECT_ROOT}"
+cd "${PROJECT_ROOT}"
+
+function setPerms {
+	mkdir -p $1
+	sudo setfacl -R  -m m:rwx -m u:33:rwX -m u:1000:rwX $1
+	sudo setfacl -dR -m m:rwx -m u:33:rwX -m u:1000:rwX $1
+}
+
+echo -e '\n## Setting up permissions ... '
+setPerms "${PROJECT_ROOT}/var/cache"
+setPerms "${PROJECT_ROOT}/var/log"
+setPerms "${PROJECT_ROOT}/var/sessions"
+setPerms "${PROJECT_ROOT}/var/jwt"
+setPerms "${PROJECT_ROOT}/var/uploads"
+
+cd "${PROJECT_ROOT}"
+
+rm -rf ./var/cache/*
+
+echo -e '\n## Composer install ... '
+composer --no-interaction config -g github-oauth.github.com 3b6e5f518a2ffb7399c2b8be8629e91f8feb5821
+time composer --no-interaction install
+
+bin/console cache:clear --env=prod
+
+if [ "${NFQ_PROJECT_INIT:-1}" = '1' ]; then
+    echo -e '\n## Generating  JWT token keys... '
+    openssl genrsa -passout pass:project -out var/jwt/private.pem -aes256 4096
+    openssl rsa -passin pass:project -pubout -in var/jwt/private.pem -out var/jwt/public.pem
+
+    echo -e '\n## DB setup ... '
+    bin/console doctrine:database:drop --force --if-exists
+    bin/console doctrine:database:create
+    bin/console doctrine:migrations:migrate --no-interaction
+    bin/console sylius:rbac:initialize
+    bin/console sylius:fixtures:load default --no-interaction
+
+
+    echo -e '\n## Testing DB setup ... '
+    bin/console doctrine:database:drop --force --if-exists --env=test
+    bin/console doctrine:database:create --env=test
+    bin/console doctrine:schema:update --force --env=test
+fi
+
+echo -e '\n## Frontend setup ... '
+yarn install
+yarn build
+
+sleep 15
+
+echo -e '\n## Clearing doctrines cache ... '
+bin/console doctrine:cache:clear-result --env=prod
+bin/console doctrine:cache:clear-query --env=prod
+bin/console doctrine:cache:clear-metadata --env=prod
+
+nc supervisor 2048
